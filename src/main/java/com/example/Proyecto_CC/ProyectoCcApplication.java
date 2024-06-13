69d8b8d991d889878448d2162e9b0892759c4e2c
Elias <Elias@DESKTOP-DQADVVG>
2024-06-13 11:53:04 -0600

    Centro de Computo

--- src/main/java/com/example/Proyecto_CC/ProyectoCcApplication.java
+++ src/main/java/com/example/Proyecto_CC/ProyectoCcApplication.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins("http://localhost:4200")
+                            .allowedOrigins("localhost:4200")
                             .allowCredentials(true)
                             .allowedHeaders("Origin","Authorization","Content-Type")
                             .allowedMethods("GET","POST","PUT","DELETE");   
