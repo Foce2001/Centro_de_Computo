d4cf7c45f29abf9a6d7fa0dbccca4ad9b53c7ff1
Elias <Elias@DESKTOP-DQADVVG>
2024-06-13 11:25:18 -0600

    Centro de Computo

--- src/main/java/com/example/Proyecto_CC/ProyectoCcApplication.java
+++ src/main/java/com/example/Proyecto_CC/ProyectoCcApplication.java
@@ -2,6 +2,9 @@ package com.example.Proyecto_CC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoCcApplication {
@@ -9,5 +12,17 @@ public class ProyectoCcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoCcApplication.class, args);
 	}
+        @Configuration
+public class WebConfig implements WebMvcConfigurer {
 
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("http://localhost:4200")
+                            .allowCredentials(true)
+                            .allowedHeaders("Origin","Authorization","Content-Type")
+                            .allowedMethods("GET","POST","PUT","DELETE");   
+        }
+    }
+        
 }
